218af86ef9195495dabbbf455f63c19fa09df320
Daniel Martins Antunes <danoan2008@gmail.com>
2018-11-03 12:06:37 -0300

    Fix on assertions in Morphology package

--- modules/components/src/Morphology.cpp
+++ modules/components/src/Morphology.cpp
@@ -18,29 +18,33 @@ void Morphology::dilate(DigitalSet &dsOut,
                         const StructuringElement se,
                         const NumberIterations it)
 {
-    Point dimIn = dsIn.domain().upperBound() - dsIn.domain().lowerBound() + Point(1,1);
+    Point lbIn,ubIn;
+    dsIn.computeBoundingBox(lbIn,ubIn);
+
+    Point dimIn = dsIn.domain().upperBound() - dsIn.domain().lowerBound()+ Point(1,1);
     int rIn = dimIn(1);
     int cIn = dimIn(0);
 
-    Point dimOut = dsOut.domain().upperBound() - dsOut.domain().lowerBound() + Point(1,1);
+    Point lbOut,ubOut;
+    lbOut = dsOut.domain().lowerBound();
+    ubOut = dsOut.domain().upperBound();
+
+    Point dimOut = ubOut - lbOut + Point(1,1);
     int rOut = dimOut(1);
     int cOut = dimOut(0);
 
-    Point lbIn, ubIn;
-    dsOut.computeBoundingBox(lbIn,ubIn);
-    Point dimInBox = ubIn - lbIn + Point(1,1);
 
-    assert(dimOut(0)>=dimInBox(0)+se.size);
-    assert(dimOut(1)>=dimInBox(1)+se.size);
+    assert(lbOut(0) <= lbIn(0) - se.size);
+    assert(lbOut(1) <= lbIn(1) - se.size);
+
+    assert(ubOut(0) >= ubIn(0) + se.size);
+    assert(ubOut(1) >= ubIn(1) + se.size);
 
 
     Point shift = dsIn.domain().lowerBound();
 
-    cv::Mat cvSrc(rIn, cIn, IMG_TYPE);
-    cvSrc = 0;
-
-    cv::Mat dilation_dst(rOut, cOut, IMG_TYPE);
-    dilation_dst = 0;
+    cv::Mat cvSrc(rOut, cOut, IMG_TYPE,cv::Scalar(0));
+    cv::Mat dilation_dst(rOut, cOut, IMG_TYPE,cv::Scalar(0));
 
     DIPaCUS::Representation::digitalSetToCVMat(cvSrc, dsIn);
 
@@ -71,27 +75,32 @@ void Morphology::erode(DigitalSet &dsOut,
                        const StructuringElement se,
                        const NumberIterations it)
 {
-    Point dimIn = dsIn.domain().upperBound() - dsIn.domain().lowerBound() + Point(1,1);
+    Point lbIn,ubIn;
+    dsIn.computeBoundingBox(lbIn,ubIn);
+
+    Point dimIn = dsIn.domain().upperBound() - dsIn.domain().lowerBound()+ Point(1,1);
     int rIn = dimIn(1);
     int cIn = dimIn(0);
 
-    Point dimOut = dsOut.domain().upperBound() - dsOut.domain().lowerBound() + Point(1,1);
+    Point lbOut,ubOut;
+    lbOut = dsOut.domain().lowerBound();
+    ubOut = dsOut.domain().upperBound();
+
+    Point dimOut = ubOut - lbOut + Point(1,1);
     int rOut = dimOut(1);
     int cOut = dimOut(0);
 
-    Point lbIn, ubIn;
-    dsOut.computeBoundingBox(lbIn,ubIn);
-    Point dimInBox = ubIn - lbIn + Point(1,1);
 
-    assert(dimOut(0)>=dimInBox(0)+se.size);
-    assert(dimOut(1)>=dimInBox(1)+se.size);
+    assert(lbOut(0) <= lbIn(0));
+    assert(lbOut(1) <= lbIn(1));
+
+    assert(ubOut(0) >= ubIn(0));
+    assert(ubOut(1) >= ubIn(1));
 
     Point shift = dsIn.domain().lowerBound();
 
-    cv::Mat cvSrc(rIn, cIn, IMG_TYPE);
-    cv::Mat erosion_dst(rOut, cOut, IMG_TYPE);
-    erosion_dst = 0;
-    cvSrc = 0;
+    cv::Mat cvSrc(rIn, cIn, IMG_TYPE, cv::Scalar(0));
+    cv::Mat erosion_dst(rIn, cIn, IMG_TYPE, cv::Scalar(0));
 
     DIPaCUS::Representation::digitalSetToCVMat(cvSrc, dsIn);
     cv::Mat element = cv::getStructuringElement(se.type,
